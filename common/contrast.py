e4b76a00dab9288962b60daf6630aeea7cf52c6a
Eyeeco <1911362549@qq.com>
2018-04-10 15:22:09 +0800

    str

--- common/contrast.py
+++ common/contrast.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def get_id_and_name(work_num, file='contrast.xls'):
-    data = pd.read_excel(file, sheetname='Sheet1',header=None)
+def get_id_and_name(work_num, file='../contrast.xls'):
+    data = pd.read_excel(file, sheetname='Sheet1',header=None).astype('str')
     target = data[data[2].isin([work_num])]
     if not target.empty :
         return target.values[0][0], target.values[0][1]
