18cbe73c698d754438dbab6e886fdcfc0b9df825
Eyeeco <1911362549@qq.com>
2018-04-19 22:00:51 +0800

    email

--- settings.py
+++ settings.py
@@ -174,17 +174,14 @@ LOGIN_REDIRECT_URL = '/'
 # DEFAULT_FROM_EMAIL = 'kyywf@dlut.edu.cn'
 # EMAIL_USE_TLS = False
 
-# EMAIL_USER = '1911362549@mail.dlut.edu.cn'
-# EMAIL_PASS = '19940308liuxiang'
-
-EMAIL_USER = 'cxlt@dlut.edu.cn'
-EMAIL_PASS = 'YL8-Aux-A9v-5Yn'
+EMAIL_USER = '1955738360@qq.com'
+EMAIL_PASS = '19940308liuxiang'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SERVER_EMAIL = EMAIL_USER
 EMAIL_SUBJECT_PREFIX = '[DlutInnovationManagement]'
-EMAIL_HOST = 'mail.dlut.edu.cn'
-EMAIL_PORT = '25'
+EMAIL_HOST = 'mail.qq.com'
+EMAIL_PORT = '587'
 EMAIL_HOST_USER = EMAIL_USER
 EMAIL_HOST_PASSWORD = EMAIL_PASS
 DEFAULT_FROM_EMAIL = EMAIL_USER
