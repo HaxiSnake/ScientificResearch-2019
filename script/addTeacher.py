13aa51da4ce87fa936c8180443a6446f7e766f72
Eyeeco <1911362549@qq.com>
2018-07-23 15:18:05 +0800

    bug

--- script/addTeacher.py
+++ script/addTeacher.py
@@ -7,23 +7,18 @@ from teacher.models import TeacherInfoSetting
 def addTeacher(id, college):
 
     user = User.objects.get(id=id)
-    try:
-        new_authority = UserIdentity.objects.get(identity=TEACHER_USER)
-        new_authority.auth_groups.add(user)
-        new_authority.save()
-    except:
-        print("new_authority--Fail")
-    try:
-        collegeObj = College.objects.get(id=college)
-        teacherProfileObj = TeacherProfile(userid=user,college=collegeObj)
-        teacherProfileObj.save()
-    except:
-        print("collegeObj--Fail")
-    try:
-        teacherInfoSettingObj = TeacherInfoSetting(teacher=teacherProfileObj)
-        teacherInfoSettingObj.card = user.username
-        teacherInfoSettingObj.name = user.first_name
-        teacherInfoSettingObj.save()
-    except:
-        print("teacherInfoSettingObj--Fail")
+
+    new_authority = UserIdentity.objects.get(identity=TEACHER_USER)
+    new_authority.auth_groups.add(user)
+    new_authority.save()
+
+    collegeObj = College.objects.get(id=college)
+    teacherProfileObj = TeacherProfile(userid=user,college=collegeObj)
+    teacherProfileObj.save()
+
+    teacherInfoSettingObj = TeacherInfoSetting(teacher=teacherProfileObj)
+    teacherInfoSettingObj.card = user.username
+    teacherInfoSettingObj.name = user.first_name
+    teacherInfoSettingObj.save()
+
     return 1
