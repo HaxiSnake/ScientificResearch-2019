9c038a54ea8f546c61f06bb30a794e8fa1cdc5c4
Eyeeco <1911362549@qq.com>
2018-07-23 14:33:36 +0800

    删除中文

--- script/addTeacher.py
+++ script/addTeacher.py
@@ -4,13 +4,13 @@ from users.models import TeacherProfile,SchoolProfile,CollegeProfile,ExpertProfi
 from teacher.models import TeacherInfoSetting
 
 def addTeacher(id, college):
-    # 创建用户
+
     user = User.objects.get(id=id)
-    # 增加用户身份
+
     new_authority = UserIdentity.objects.get(identity=TEACHER_USER)
     new_authority.auth_groups.add(user)
     new_authority.save()
-    # 增加老师记录
+
     collegeObj = College.objects.get(id=college)
     teacherProfileObj = TeacherProfile(userid=user,college=collegeObj)
     teacherProfileObj.save()
