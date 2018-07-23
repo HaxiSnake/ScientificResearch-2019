1d2769197cde59aa41054de4f50328017cc6f975
Eyeeco <1911362549@qq.com>
2018-07-23 14:44:43 +0800

    try_catch

--- script/addTeacher.py
+++ script/addTeacher.py
@@ -7,16 +7,23 @@ from teacher.models import TeacherInfoSetting
 def addTeacher(id, college):
 
     user = User.objects.get(id=id)
-
-    new_authority = UserIdentity.objects.get(identity=TEACHER_USER)
-    new_authority.auth_groups.add(user)
-    new_authority.save()
-
-    collegeObj = College.objects.get(id=college)
-    teacherProfileObj = TeacherProfile(userid=user,college=collegeObj)
-    teacherProfileObj.save()
-    teacherInfoSettingObj = TeacherInfoSetting(teacher=teacherProfileObj)
-    teacherInfoSettingObj.card = user.username
-    teacherInfoSettingObj.name = user.first_name
-    teacherInfoSettingObj.save()
+    try:
+        new_authority = UserIdentity.objects.get(identity=TEACHER_USER)
+        new_authority.auth_groups.add(user)
+        new_authority.save()
+    except:
+        print("new_authority--Fail")
+    try:
+        collegeObj = College.objects.get(id=college)
+        teacherProfileObj = TeacherProfile(userid=user,college=collegeObj)
+        teacherProfileObj.save()
+    except:
+        print("collegeObj--Fail")
+    try:
+        teacherInfoSettingObj = TeacherInfoSetting(teacher=teacherProfileObj)
+        teacherInfoSettingObj.card = user.username
+        teacherInfoSettingObj.name = user.first_name
+        teacherInfoSettingObj.save()
+    except:
+        print("teacherInfoSettingObj--Fail")
     return 1
