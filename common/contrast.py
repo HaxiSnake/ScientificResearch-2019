5aed2dfee0a1fd1aaa40e5c3bb1175b26ada9c8c
Eyeeco <1911362549@qq.com>
2018-12-24 19:35:09 +0800

    修改认证方式

--- common/contrast.py
+++ common/contrast.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import sys
+from const.models import UserTrans
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-def get_id_and_name(work_num, file='contrast.xls'):
-    data = pd.read_excel(file, sheet_name='Sheet1',header=None).astype('str')
-    target = data[data[2].isin([work_num])]
-    if not target.empty :
-        return target.values[0][0]
-    else :
+# def get_id_and_name(work_num, file='contrast.xls'):
+#     data = pd.read_excel(file, sheet_name='Sheet1',header=None).astype('str')
+#     target = data[data[2].isin([work_num])]
+#     if not target.empty :
+#         return target.values[0][0]
+#     else :
+#         return None
+#
+
+def get_id_and_name(work_num):
+    user = UserTrans.objects.get(work_number = work_num)
+    if user:
+        return user.id_number
+    else:
         return None
